85996cce0b6d232f43d627d53bd9a9e1f74f1969
T460 <jacobgeorge08@gmail.com>
2026-07-08 10:07:55 +0530

    worked through first 11 sections of rustlings

---
+++ exercises/10_modules/modules1.rs
@@ -0,0 +1,15 @@
+pub(crate) mod sausage_factory {
+    // Don't let anybody outside of this module see this!
+    fn get_secret_recipe() -> String {
+        String::from("Ginger")
+    }
+
+    pub(crate) fn make_sausage() {
+        get_secret_recipe();
+        println!("sausage!");
+    }
+}
+
+fn main() {
+    sausage_factory::make_sausage();
+}
