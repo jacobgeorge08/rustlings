fc1d966125ab0dc16b99458cd2b24954dc4d4aba
T460 <jacobgeorge08@gmail.com>
2026-07-11 18:58:06 +0530

    worked through errors exercises

---
+++ exercises/13_error_handling/errors4.rs
@@ -0,0 +1,54 @@
+#[derive(PartialEq, Debug)]
+enum CreationError {
+    Negative,
+    Zero,
+}
+
+#[derive(PartialEq, Debug)]
+struct PositiveNonzeroInteger(u64);
+
+impl PositiveNonzeroInteger {
+    fn new(value: i64) -> Result<Self, CreationError> {
+        // if value < 0 {
+        //     Err(CreationError::Negative)
+        // } else if value == 0 {
+        //     Err(CreationError::Zero)
+        // } else {
+        //     Ok(Self(value as u64))
+        // }
+
+        match value {
+            i64::MIN..=-1 => Err(CreationError::Negative),
+            0 => Err(CreationError::Zero),
+            _ => Ok(Self(value as u64)),
+        }
+
+        // match value.cmp(&0) {
+        //     std::cmp::Ordering::Less => Err(CreationError::Negative),
+        //     std::cmp::Ordering::Greater => Ok(Self(value as u64)),
+        //     std::cmp::Ordering::Equal => Err(CreationError::Zero),
+        // }
+    }
+}
+
+fn main() {
+    // You can optionally experiment here.
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_creation() {
+        assert_eq!(
+            PositiveNonzeroInteger::new(10),
+            Ok(PositiveNonzeroInteger(10)),
+        );
+        assert_eq!(
+            PositiveNonzeroInteger::new(-10),
+            Err(CreationError::Negative),
+        );
+        assert_eq!(PositiveNonzeroInteger::new(0), Err(CreationError::Zero));
+    }
+}
